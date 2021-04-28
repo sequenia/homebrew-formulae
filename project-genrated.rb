256ff92325bc6458ffa83355b27ff039e46454e1
Ivan Michaylovsky <ivan.michaylovsky@sequenia.com>
2021-04-28 17:57:39 +0700

    add projectGenerate rb

---
+++ project-genrated.rb
@@ -0,0 +1,18 @@
+class FigmaExport < Formula
+  desc 'Tool for project genrated for xcode'
+  homepage 'https://github.com/sequenia/generate-project.git'
+  url 'https://github.com/sequenia/generate-project.git', tag: '0.1'
+  version '0.1'
+  license 'MIT'
+
+  depends_on xcode: ['10.0', :build]
+
+  def install
+    system 'make', 'build'
+    bin.install '.build/release/projectGenrated'
+  end
+
+  test do
+    system "#{bin}/projectGenrated gen"
+  end
+end
