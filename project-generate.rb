b348bfb142a6682ac77c047ed51193cec108967a
Ivan Michaylovsky <ivan.michaylovsky@sequenia.com>
2021-04-29 15:42:29 +0700

    up version

--- project-generate.rb
+++ project-generate.rb
@@ -1,8 +1,8 @@
 class ProjectGenerate < Formula
   desc 'Tool for project generate for xcode'
   homepage 'https://github.com/sequenia/generate-project.git'
-  url 'https://github.com/sequenia/generate-project.git', tag: '0.1.5'
-  version '0.1.5'
+  url 'https://github.com/sequenia/generate-project.git', tag: '0.1.6'
+  version '0.1.6'
   license 'MIT'
 
   depends_on xcode: ['10.0', :build]
