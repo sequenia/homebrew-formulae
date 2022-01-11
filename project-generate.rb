341a9abb4fce8db81c8f2ba7124d717cf16f812e
Semen Kologrivov <semen@sequenia.com>
2022-01-11 13:25:10 +0700

    Update project-generate.rb

--- project-generate.rb
+++ project-generate.rb
@@ -2,7 +2,7 @@ class ProjectGenerate < Formula
   desc 'Tool for project generate for xcode'
   homepage 'https://github.com/sequenia/generate-project.git'
   url 'https://github.com/sequenia/generate-project.git', tag: '0.5.1'
-  version '0.1.10'
+  version '0.5.1'
   license 'MIT'
 
   depends_on xcode: ['10.0', :build]
