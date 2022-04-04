0d8bab7b61a7b03ca5c088b8088503d6b4909d65
Semen Kologrivov <semen@sequenia.com>
2022-04-04 10:08:31 +0700

    Update figma-export.rb

--- figma-export.rb
+++ figma-export.rb
@@ -1,8 +1,8 @@
 class FigmaExport < Formula
   desc 'Tool for exporting resources from Figma: icons, colors, fonts'
   homepage 'https://github.com/sequenia/figma-export'
-  url 'https://github.com/sequenia/figma-export.git', tag: '0.7.0'
-  version '0.7.0'
+  url 'https://github.com/sequenia/figma-export.git', tag: '0.31.0'
+  version '0.31.0'
   license 'MIT'
 
   depends_on xcode: ['10.0', :build]
